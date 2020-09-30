0149d527880054f991cb03c5a3a36753750c6d1f
Капралов Яков Владиславович <70843236+prorockk@users.noreply.github.com>
2020-09-30 20:15:34 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Boolean(array)==true && array.length!=0){return Math.min.apply(null, array)}
+  else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) { 
+  if (Boolean(array)==true && array.length!=0){return Math.max.apply(null, array)}
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (Boolean(array) && array.length!=0) {
+  for (i=0; i<array.length; i++) {
+    sum +=array[i]; 
+    }
+    if (sum!=0) {return sum/array.length}
+    else return 0;
+  }
+  else return 0;
+  
 }
